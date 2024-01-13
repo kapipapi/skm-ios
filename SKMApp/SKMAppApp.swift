bfe3440e7bc179cf65b009b535609ba80ce79a82
Kacper Ledwosiński <kl@airotec.pl>
2024-01-13 12:08:09 +0100

    Initial Commit

---
+++ SKMApp/SKMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKMAppApp.swift
+//  SKMApp
+//
+//  Created by Kacper Ledwosiński on 13/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SKMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
